8d00b88e9b32a2278b2b45a6623dd68372a67190
itsparasbisht <iamparasbisht@gmail.com>
2023-03-06 20:30:43 +0530

    changes

--- script.ts
+++ script.ts
@@ -3,6 +3,23 @@ const prisma = new PrismaClient();
 
 async function main() {
   await prisma.user.deleteMany();
+  const user = await prisma.user.create({
+    data: {
+      name: "Paras",
+      age: 10,
+      email: "paras@mail.com",
+      userPreference: {
+        create: {
+          emailUpdates: true,
+        },
+      },
+    },
+    select: {
+      name: true,
+    },
+  });
+
+  console.log(user);
 }
 
 main();
